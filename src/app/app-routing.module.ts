67190584e69e23f6e92dc52cfe71d84b85ef7076
oAlexChaves <68710954+oAlexChaves@users.noreply.github.com>
2022-04-17 14:08:36 -0300

    reset ao blank

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'sidenav',
+    loadChildren: () => import('./sidenav/sidenav.module').then( m => m.SidenavPageModule)
+  },
 ];
 
 @NgModule({
